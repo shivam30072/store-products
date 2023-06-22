727a3200fef98699e7ba33154e8b07e6b09d9130
shivam30072 <shivam30072@gmail.com>
2023-06-22 23:15:09 +0530

    DB setup

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-require("dotenv").config;
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -23,9 +24,10 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
-const start = () => {
+const start = async () => {
   try {
     //connect db
+    await connectDB(process.env.MONGO_URI);
     app.listen(PORT, console.log(`server is running on port ${PORT}`));
   } catch (error) {
     console.log(error);
