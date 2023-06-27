956a841fa22d319938bbcb1602e13d069871b373
shivam30072 <shivam30072@gmail.com>
2023-06-27 23:42:33 +0530

    practicing different mongoose method

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,36 @@
 const product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await product.find({ company: "ikea" });
+  // req.query for getting query values
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    // queries always comes in string make sure to convert it into actual type
+    queryObject["featured"] = featured === "false" ? false : true;
+  }
+
+  const products = await product.find(queryObject);
+  // hardcoded way not a good approach
+  // const products = await product.find({ featured: false });
   res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  // sort method for sorting in ascending order
+  // const products = await product.find({}).sort("price name");
+
+  // sort method sorting in descending order
+  // const products = await product.find({}).sort("-price -name");
+
+  // select method only gets you the field that you specify
+  // const products = await product.find({}).select("price name company");
+
+  // limit method to limit the number of results
+  // const products = await product.find({}).limit(10);
+
+  // skip method to skip the results
+  const products = await product.find({}).skip(10);
+  res.status(200).json({ total: products.length, products });
 };
 
 module.exports = {
